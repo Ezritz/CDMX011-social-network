916ca2796d84188112e9430deb0bb52255f60181
ezritz <lidia.edith.escoto@gmail.com>
2021-09-07 22:18:38 -0500

    Add firebase config

--- src/firebaseClient.js
+++ src/firebaseClient.js
@@ -1,12 +1,5 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyDVGtWJFczNYd3wgIjbhqz5Q3YOW7OW-Dc',
-  authDomain: 'labo-social-net.firebaseapp.com',
-  projectId: 'labo-social-net',
-  storageBucket: 'labo-social-net.appspot.com',
-  messagingSenderId: '665927646387',
-  appId: '1:665927646387:web:76d0e359d37635f99844c6',
-  measurementId: 'G-4NWK8XLQ8J',
-};
+import { firebaseConfig } from './firebase.config.js';
+
   // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
